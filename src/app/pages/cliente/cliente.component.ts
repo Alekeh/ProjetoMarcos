fad539734e030fbf35b6d52a44b5de15427a1de9
Bruno <bruno.pereira@sonner.com.br>
2020-11-25 23:31:58 -0300

    Esqueleto projeto criado Telas principais

--- src/app/pages/cliente/cliente.component.ts
+++ src/app/pages/cliente/cliente.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-cliente-component',
   templateUrl: './cliente.component.html',
   styleUrls: ['./cliente.component.scss']
 })
-export class ClienteComponent {
+export class ClienteComponent implements OnInit {
+  constructor(private route: Router) {
+  }
+
   title = 'ProjetoAngular';
+
+  ngOnInit(): void {
+  }
 }
